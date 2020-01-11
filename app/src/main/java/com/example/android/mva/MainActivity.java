663b01f6ede1f5a8a70f7fa7fb6a05b6750e9c15
helixjolt <siddharth.byale17@siesgst.ac.in>
2020-01-11 14:33:19 +0530

    Color scheme updated

--- app/src/main/java/com/example/android/mva/MainActivity.java
+++ app/src/main/java/com/example/android/mva/MainActivity.java
@@ -10,7 +10,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.support.v4.app.Fragment;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
@@ -19,6 +18,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
