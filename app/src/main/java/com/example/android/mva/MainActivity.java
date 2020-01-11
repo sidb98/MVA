8b938d2efb66ff8d5318f3e2b63071f6a51a46ad
Aditya Parthasarathy <adityap301@gmail.com>
2020-01-11 15:40:02 +0530

    Added viewFlipper to navBar
    Merged Aakash's code and Siddharth's code. Navbar and viewFlipper
    working fine now.

--- app/src/main/java/com/example/android/mva/MainActivity.java
+++ app/src/main/java/com/example/android/mva/MainActivity.java
@@ -10,16 +10,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ViewFlipper;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     private DrawerLayout drawer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -64,4 +66,5 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             super.onBackPressed();
         }
     }
+
 }
--- app/src/main/java/com/example/android/mva/VisualAcuity.java
+++ app/src/main/java/com/example/android/mva/VisualAcuity.java
@@ -7,11 +7,43 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ViewFlipper;
 
-public class VisualAcuity extends Fragment {
+public class VisualAcuity extends Fragment implements View.OnClickListener {
+    ViewFlipper viewFlipper;
+    Button b1,b2,b3,b4;
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_visual_acuity, container, false);
+    public View onCreateView (@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View visualAcuityView =  inflater.inflate(R.layout.fragment_visual_acuity, container, false);
+
+        viewFlipper=visualAcuityView.findViewById(R.id.ViewFlipper);
+        b1= visualAcuityView.findViewById(R.id.button_right);
+        b2= visualAcuityView.findViewById(R.id.button_left);
+        b3= visualAcuityView.findViewById(R.id.button_top);
+        b4= visualAcuityView.findViewById(R.id.button_bottom);
+
+        b1.setOnClickListener(this);
+        b2.setOnClickListener(this);
+        b3.setOnClickListener(this);
+        b4.setOnClickListener(this);
+
+        return visualAcuityView;
+    }
+    @Override
+    public void onClick(View v) {
+        if (v == b1) {
+            viewFlipper.showNext();
+        } else if (v == b2) {
+            viewFlipper.showNext();
+        } else if (v == b3) {
+            viewFlipper.showNext();
+        } else if (v == b4) {
+            viewFlipper.showNext();
+        }
+
+
     }
 }
