e7bc01fc80814aab07a730add981fd464e787627
helixjolt <siddharth.byale17@siesgst.ac.in>
2020-01-11 19:25:27 +0530

    Button position updated

--- app/src/main/java/com/example/android/mva/MainActivity.java
+++ app/src/main/java/com/example/android/mva/MainActivity.java
@@ -4,15 +4,12 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ViewFlipper;
+
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     private DrawerLayout drawer;
